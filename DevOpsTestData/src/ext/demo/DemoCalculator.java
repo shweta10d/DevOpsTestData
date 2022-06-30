0e4df1a03edd07ab2113410fe0cb847b9811fda4
shweta.v.deshpande <shweta.v.deshpande@BDC6C-L-210J0Q4.dir.svc.accenture.com>
2022-06-30 18:38:06 +0530

    commiting eclipse project

---
+++ DevOpsTestData/src/ext/demo/DemoCalculator.java
@@ -0,0 +1,13 @@
+package ext.demo;
+
+public class DemoCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=10;
+		int y=34;
+		System.out.println("The Result is:"+x*y);	
+		}
+
+}
---
+++ DevOpsTestData/src/module-info.java
@@ -0,0 +1,2 @@
+module DevOpsTestData {
+}
